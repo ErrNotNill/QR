3a6f32e79ddfe3843110d90cdc2b4cc1d4fce13e
ErrNotNill <andreevivan8891@gmail.com>
2023-10-29 15:35:31 +0300

    fix auth amo

--- amocrm/auth.go
+++ amocrm/auth.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/alexeykhan/amocrm"
 	"io"
 	"log"
 	"net/http"
@@ -16,23 +17,79 @@ type Token struct {
 	RefreshToken string `json:"refresh_token"`
 }
 
+func NewAuth() {
+	subdomain := "onvizbitrix"
+	link := "https://" + subdomain + ".amocrm.ru/oauth2/access_token"
+
+	data := map[string]string{
+		"client_id":     "250e6632-c2d2-4986-93a1-e9bd16002327",
+		"client_secret": "f6edVVFGIZKEPiAabNTknkALBsqdmTpYAPRD0WEixrA6BLtIA5WMZIQnFZKaT1xT",
+		"grant_type":    "authorization_code",
+		"code":          "def5020007223ff71807070f757754801e4b250513c1f6e9981a010860d94370fc57567ba043d8c88df99e7860a4b80c9cb7a885fa0c006647033adbdf7d93698df1045c8b8f5f5d96d5b2e135da14e8ef7e6311458429f9c7de9681c0d79c4d12bd55f28430878982543ddf2773cead1f4fc6bccb83c7dba1af1feddc0b4b2b9322c81a401b31148dc95556ee0554bf2b138bd595888472f9452da4b8bfea407292267a225b7b0f2575a1015fa35765103a792e63f276a795e0132211e2cab2f71267354e937ce3e62f265efee557239acdda66e8e10ce64f41d3c4e4de553cc5d1a932021d07766a6f13699a1e0774c233afa059cd4618c097961dcc642811124eafe70245ceb90ca933651ba527fefe03aabd698c257d56c863d8f41b70a28646fbd65d3d9867d9af31fce8d72f81561847013fe6caa2d3d8302b8c2155bfce4952d2bb641082abb0fad6d782ff8a5964acbb5945f79fc763e0938b2f2b67b33e977ad7cc3d1b2bd727cabd1baf6c1fc8812acc901e4afd3c77f7c755cb888d3a74e05fdc18733e1b1fbd44195bda228db5dd71f6684f491ba7da2c3e3da0a7063fadd78716f286fe2f54fed44ba53d1a958be9fdc0cf2f59c8f2bcf7e792442833cef319946967111ae96b47905be331d90b3406ea0ba4e77a525387e33e81c9e42c87a2a067852bb8aa630745",
+		"redirect_uri":  "https://onviz-api.ru/redirect",
+	}
+
+	payload, _ := json.Marshal(data)
+
+	req, err := http.NewRequest("POST", link, bytes.NewBuffer(payload))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("User-Agent", "amoCRM-oAuth-client/1.0")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode > 204 {
+		fmt.Println("Error:", resp.Status)
+		return
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var response map[string]interface{}
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	access_token, _ := response["access_token"].(string)
+	refresh_token, _ := response["refresh_token"].(string)
+	token_type, _ := response["token_type"].(string)
+	expires_in, _ := response["expires_in"].(float64)
+
+	fmt.Println("Access Token:", access_token)
+	fmt.Println("Refresh Token:", refresh_token)
+	fmt.Println("Token Type:", token_type)
+	fmt.Println("Expires In:", expires_in)
+}
+
 func GetToken() {
 	uri := `https://onvizbitrix.amocrm.ru/oauth2/access_token`
-	clientID := "250e6632-c2d2-4986-93a1-e9bd16002327"
-	clientSecret := "f6edVVFGIZKEPiAabNTknkALBsqdmTpYAPRD0WEixrA6BLtIA5WMZIQnFZKaT1xT"
-	authCode := "def5020007223ff71807070f757754801e4b250513c1f6e9981a010860d94370fc57567ba043d8c88df99e7860a4b80c9cb7a885fa0c006647033adbdf7d93698df1045c8b8f5f5d96d5b2e135da14e8ef7e6311458429f9c7de9681c0d79c4d12bd55f28430878982543ddf2773cead1f4fc6bccb83c7dba1af1feddc0b4b2b9322c81a401b31148dc95556ee0554bf2b138bd595888472f9452da4b8bfea407292267a225b7b0f2575a1015fa35765103a792e63f276a795e0132211e2cab2f71267354e937ce3e62f265efee557239acdda66e8e10ce64f41d3c4e4de553cc5d1a932021d07766a6f13699a1e0774c233afa059cd4618c097961dcc642811124eafe70245ceb90ca933651ba527fefe03aabd698c257d56c863d8f41b70a28646fbd65d3d9867d9af31fce8d72f81561847013fe6caa2d3d8302b8c2155bfce4952d2bb641082abb0fad6d782ff8a5964acbb5945f79fc763e0938b2f2b67b33e977ad7cc3d1b2bd727cabd1baf6c1fc8812acc901e4afd3c77f7c755cb888d3a74e05fdc18733e1b1fbd44195bda228db5dd71f6684f491ba7da2c3e3da0a7063fadd78716f286fe2f54fed44ba53d1a958be9fdc0cf2f59c8f2bcf7e792442833cef319946967111ae96b47905be331d90b3406ea0ba4e77a525387e33e81c9e42c87a2a067852bb8aa630745"
-	redirectUri := "https://onviz-api.ru/redirect"
 
-	bodyStr := fmt.Sprintf(`{
-  "client_id": "%s",
-  "client_secret": "%s",
-  "grant_type": "authorization_code",
-  "code": "%s",
-  "redirect_uri": "%s"
-}`, clientID, clientSecret, authCode, redirectUri)
+	data := map[string]string{
+		"client_id":     "250e6632-c2d2-4986-93a1-e9bd16002327",
+		"client_secret": "f6edVVFGIZKEPiAabNTknkALBsqdmTpYAPRD0WEixrA6BLtIA5WMZIQnFZKaT1xT",
+		"grant_type":    "authorization_code",
+		"code":          "def5020007223ff71807070f757754801e4b250513c1f6e9981a010860d94370fc57567ba043d8c88df99e7860a4b80c9cb7a885fa0c006647033adbdf7d93698df1045c8b8f5f5d96d5b2e135da14e8ef7e6311458429f9c7de9681c0d79c4d12bd55f28430878982543ddf2773cead1f4fc6bccb83c7dba1af1feddc0b4b2b9322c81a401b31148dc95556ee0554bf2b138bd595888472f9452da4b8bfea407292267a225b7b0f2575a1015fa35765103a792e63f276a795e0132211e2cab2f71267354e937ce3e62f265efee557239acdda66e8e10ce64f41d3c4e4de553cc5d1a932021d07766a6f13699a1e0774c233afa059cd4618c097961dcc642811124eafe70245ceb90ca933651ba527fefe03aabd698c257d56c863d8f41b70a28646fbd65d3d9867d9af31fce8d72f81561847013fe6caa2d3d8302b8c2155bfce4952d2bb641082abb0fad6d782ff8a5964acbb5945f79fc763e0938b2f2b67b33e977ad7cc3d1b2bd727cabd1baf6c1fc8812acc901e4afd3c77f7c755cb888d3a74e05fdc18733e1b1fbd44195bda228db5dd71f6684f491ba7da2c3e3da0a7063fadd78716f286fe2f54fed44ba53d1a958be9fdc0cf2f59c8f2bcf7e792442833cef319946967111ae96b47905be331d90b3406ea0ba4e77a525387e33e81c9e42c87a2a067852bb8aa630745",
+		"redirect_uri":  "https://onviz-api.ru/redirect",
+	}
 
-	body := []byte(bodyStr)
-	req, err := http.NewRequest("POST", uri, bytes.NewReader(body))
+	payload, _ := json.Marshal(data)
+	req, err := http.NewRequest("POST", uri, bytes.NewBuffer(payload))
 	if err != nil {
 		log.Println("error creating request")
 	}
@@ -64,12 +121,19 @@ func GetToken() {
 	log.Println("response:", token)*/
 }
 
-/*func CreateAmoClient() {
-	accessToken := "def50200afc33242d5f17bb037ff864ab9cd9cf9f318a052647f3d3b22e63e58cf53a6f3ff58ec54b6610dcc7c9378b159a7a56e347ac39afe83a3c33db0a98fa18c9f0ec5eefc7aa1a5babe431f9643ed930982cfaf16fc1fbeba6d27ffaf0e43881c7c9bb1f174b3ddbc5593c0a827fde632d73924f97fbde77ddeca1e08f3d0211f64dc308f70904ea5007a78e62d39f38d82a78ab9be3bf61e6080a21f16d1e08e2720ab079ec11b6242e2e9d22bc2c53339c1af71dd3b9a998fea72618edaefe5a597b45d9824e2b671551afb145dbf3f4acfd96cf9baedd759b664f8aac96b5da9c38a1f7b7044039952e5ffb3d153bc334a829d440bdc68272935e5455294491f48b63bb18dcc0868fbd43ec1707966c2bdb005ad9cd9703534ccb8ceaf411d5932caa903004625450ed680127b308ff1adbc19d9fcdc505a83c3f0e7651b6423ff3393683723acc176b0039f0039ac9cc3e84b4f730159b23ac7ceac05f903b589f4f5456ed6adaa4bfdcb624537650258f8e7ad0513bef23237388f4ffe61b0d8fba2d23d1b26da33241c838d89132f6cb40839c6a7de61157ae33224a32f6973767dd2e36251fe0cfcabbf2b3960b00c0176d39a289f79020d25b894d949caade8b725452070539385c5708d785b97840abdf54f11700be9f3c17b0556bd7ac02b8429072b9c60e71cb0"
-	amoCRM := amocrm.New("dc7037a7-7f4e-4c5c-bbad-a93d1b3774d5", "fhieUsfUTyKmlROK7KeumLDQLb6VvCc1c7WI2f8GHo8xmpLhyrgLN1GwPsUR4CCB", "https://onviz-api.ru/amo_deal")
-	state := amocrm.RandomState()  // store this state as a session identifier
+func CreateAmoClient() {
+
+	clientID := "250e6632-c2d2-4986-93a1-e9bd16002327"
+	clientSecret := "f6edVVFGIZKEPiAabNTknkALBsqdmTpYAPRD0WEixrA6BLtIA5WMZIQnFZKaT1xT"
+	//grantType := "grant_type":    "authorization_code",
+	//authCode :=  "def5020007223ff71807070f757754801e4b250513c1f6e9981a010860d94370fc57567ba043d8c88df99e7860a4b80c9cb7a885fa0c006647033adbdf7d93698df1045c8b8f5f5d96d5b2e135da14e8ef7e6311458429f9c7de9681c0d79c4d12bd55f28430878982543ddf2773cead1f4fc6bccb83c7dba1af1feddc0b4b2b9322c81a401b31148dc95556ee0554bf2b138bd595888472f9452da4b8bfea407292267a225b7b0f2575a1015fa35765103a792e63f276a795e0132211e2cab2f71267354e937ce3e62f265efee557239acdda66e8e10ce64f41d3c4e4de553cc5d1a932021d07766a6f13699a1e0774c233afa059cd4618c097961dcc642811124eafe70245ceb90ca933651ba527fefe03aabd698c257d56c863d8f41b70a28646fbd65d3d9867d9af31fce8d72f81561847013fe6caa2d3d8302b8c2155bfce4952d2bb641082abb0fad6d782ff8a5964acbb5945f79fc763e0938b2f2b67b33e977ad7cc3d1b2bd727cabd1baf6c1fc8812acc901e4afd3c77f7c755cb888d3a74e05fdc18733e1b1fbd44195bda228db5dd71f6684f491ba7da2c3e3da0a7063fadd78716f286fe2f54fed44ba53d1a958be9fdc0cf2f59c8f2bcf7e792442833cef319946967111ae96b47905be331d90b3406ea0ba4e77a525387e33e81c9e42c87a2a067852bb8aa630745"
+	redirectUri := "https://onviz-api.ru/amo_deal"
+
+	accessToken := "def50200836cea7fdf4571649c804979a65dd5b0776fc8201c8c66120670303e5b052505e98ea22e5d5ac9b75881fffef780cd1175257ee049afc3132e8b1faca7ea95d31657546ea50b878238b31bd6593bc4ded65d79a11fe5d637472d820766a40547d24f789c83fca6c1da65b06635a21cfa93256630d02cf8cdc0e9896415f5e1f7143fd1a34a76d97600f32de3ab95086a85a58d3dee54451601f37b52d44bd5ca8762906804831f4a94f666b662dcca60bcbfc682f12965d24947d3a0c8708c9fed28c69008193a1bbafb28c301df33033b397a21dbb581ab9cbd982394146e25b2b906ebb13d7cade17e371ba5191c4ba1f3f40cee65d6e0f88f65765c8113fcfa057e763bcc5a0212eff36a05d98e56907d5de1d1b97a6ff791e7787fee41ce0416f1055526f562bc9cc2da9ad7a4091623289bc48ed4ff801818980d795c9de4628a8c0f3e8d5fe9b9fb0d5a7dcf16b25e3c9ce7c5fead6802d30ab664230f02345043bb27ee6d8d6e1496dcb4eb9ad40f9904f8178f1962bbcfccebd2a769f40bfcfff211293a8e079a36d8dd3116d65896e6ba00dfae86927902923ef64ed871ac75c7ca794bceddac26d8f5c424839d945e2ff672866fd27da10d22fd7d8c5cb16cd002fcb99e956cbbb0f8b7e7f7d5f79b9f08a777a169a329a6da1c7759e71dd1488f1099e4cf21"
+	amoCRM := amocrm.New(clientID, clientSecret, redirectUri)
+	//state := amocrm.RandomState()  // store this state as a session identifier
 	mode := amocrm.PostMessageMode // options: PostMessageMode, PopupMode
-	authURL, err := amoCRM.AuthorizeURL(state, mode)
+	authURL, err := amoCRM.AuthorizeURL("8d09edf332cf73a06bb3b6ca95cbf1d0", mode)
 	if err != nil {
 		fmt.Println("failed to get auth url:", err)
 		return
@@ -89,7 +153,7 @@ func GetToken() {
 	fmt.Println("refresh_token:", token.RefreshToken())
 	fmt.Println("token_type:", token.TokenType())
 	fmt.Println("expires_at:", token.ExpiresAt().Unix())
-}*/
+}
 
 func RedirectHandler(w http.ResponseWriter, r *http.Request) {
 	var token Token
--- go.mod
+++ go.mod
@@ -3,4 +3,9 @@ module qr
 go 1.20
 
 require github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
-require github.com/alexeykhan/amocrm v0.1.0 // indirect
+
+require (
+	github.com/alexeykhan/amocrm v0.1.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lobz1g/amocrm v1.0.1 // indirect
+)
